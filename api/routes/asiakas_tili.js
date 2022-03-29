89f0f78f4414d4be3a0f145a897f123606c91fa7
t33k00 <tmkyllonen@gmail.com>
2022-03-29 11:54:30 +0300

    kokeillaas

---
+++ api/routes/asiakas_tili.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const asiakas_tili = require('../models/asiakas_tili_model');
+
+router.get('/:id?',
+ function(request, response) {
+  if (request.params.id) {
+    asiakas_tili.getById(request.params.id, function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult);
+      }
+    });
+  } else {
+    asiakas_tili.getAll(function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult);
+      }
+    });
+  }
+});
+
+
+router.post('/', 
+function(request, response) {
+  asiakas_tili.add(request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(request.body);
+    }
+  });
+});
+
+
+router.delete('/:id', 
+function(request, response) {
+  asiakas_tili.delete(request.params.id, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+
+router.put('/:id', 
+function(request, response) {
+  asiakas_tili.update(request.params.id, request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+module.exports = router;
