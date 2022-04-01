50ada5d82636037e6fa8b762d5845359ec61b45a
t33k00 <tmkyllonen@gmail.com>
2022-04-01 14:22:41 +0300

    usermodelin pin cryptaus pimeenä, muuten ok

--- api/models/Tilitapahtumat_model.js
+++ api/models/Tilitapahtumat_model.js
@@ -2,25 +2,25 @@ const db = require('../database');
 
 const Tilitapahtumat = {
   getById: function(id, callback) {
-    return db.query('select * from Tilitapahtumat where id_Tilitapahtumat=?', [id], callback);
+    return db.query('select * from Tilitapahtumat where idTilitapahtumat=?', [id], callback);
   },
   getAll: function(callback) {
     return db.query('select * from Tilitapahtumat', callback);
   },
   add: function(Tilitapahtumat, callback) {
     return db.query(
-      'insert into Tilitapahtumat (name,author,isbn) values(?,?,?)',
-      [Tilitapahtumat.name, Tilitapahtumat.author, Tilitapahtumat.isbn],
+      'insert into Tilitapahtumat (Tili-idTili, PVM, Kellonaika, Saldonmuutos) values(?,?,?,?)',
+      [ Tilitapahtumat.Tili_idTili, Tilitapahtumat.PVM, Tilitapahtumat.Kellonaika, Tilitapahtumat.Saldonmuutos],
       callback
     );
   },
   delete: function(id, callback) {
-    return db.query('delete from Tilitapahtumat where id_Tilitapahtumat=?', [id], callback);
+    return db.query('delete from Tilitapahtumat where idTilitapahtumat=?', [id], callback);
   },
   update: function(id, Tilitapahtumat, callback) {
     return db.query(
-      'update Tilitapahtumat set name=?,author=?, isbn=? where id_Tilitapahtumat=?',
-      [Tilitapahtumat.name, Tilitapahtumat.author, Tilitapahtumat.isbn, id],
+      'update Tilitapahtumat set Tili_idTili=?, PVM=?, Kellonaika=?, Saldonmuutos=? where idTilitapahtumat=?',
+      [Tilitapahtumat.Tili_idTili, Tilitapahtumat.PVM, Tilitapahtumat.Kellonaika, Tilitapahtumat.Saldonmuutos, id],
       callback
     );
   }
