1bab635ae375837b5953eafe9768f06de9cf72ff
t33k00 <tmkyllonen@gmail.com>
2022-04-12 13:57:46 +0300

    exeä eteenpäin

--- bankautomat/bankautomat.pro
+++ bankautomat/bankautomat.pro
@@ -9,13 +9,16 @@ CONFIG += c++11
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    drawmoney.cpp \
     main.cpp \
     mainwindow.cpp
 
 HEADERS += \
+    drawmoney.h \
     mainwindow.h
 
 FORMS += \
+    drawmoney.ui \
     mainwindow.ui
 
 # Default rules for deployment.
--- bankautomat/mainwindow.h
+++ bankautomat/mainwindow.h
@@ -2,20 +2,82 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "drawmoney.h"
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
+
+enum states{
+    login,
+    mainscreen,
+    acchistory,
+    balance,
+    draw,
+    credit,
+    debit,
+    start
+};
+enum events{
+    userlogin,
+    usebankactions,
+    usenext10,
+    useprev10,
+    usedraw,
+    usecredit,
+    usedebit,
+    userlogout,
+    usebalance,
+    back
+};
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+public slots:
+    void runstatemachine(states,events);
+
+
+private slots:
+    void on_btnlogin_clicked();
+
+    void on_btnactions_clicked();
+
+    void on_btnnext_clicked();
+
+    void on_btnprev_clicked();
+
+    void on_btndraw_clicked();
+
+    void on_btncredit_clicked();
+
+    void on_btndebit_clicked();
+
+    void on_btnlogout_clicked();
+
+    void on_btnback_clicked();
+
+    void on_btnbalance_clicked();
 
 private:
     Ui::MainWindow *ui;
+    states state = start;
+    events event;
+    void loginhandler(events e);
+    void mainhandler(events e);
+    void historyhandler(events e);
+    void balancehandler(events e);
+    void drawhandler(events e);
+    void credithandler(events e);
+    void debithandler(events e);
+    void starthandler(events e);
+
 };
 #endif // MAINWINDOW_H
